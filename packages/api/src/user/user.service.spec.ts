c140d6f9c7e11aed9575e5008eee853e3b32d131
chenc <double_cl@163.com>
2022-08-14 17:20:02 +0800

    feat: add user service test case

--- packages/api/src/user/user.service.spec.ts
+++ packages/api/src/user/user.service.spec.ts
@@ -19,7 +19,22 @@ describe('UserService', () => {
     service = module.get<UserService>(UserService);
   });
 
+  const create = async () => {
+    return await service.createUser({ username: 'chenc', password: 'chenc' });
+  };
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be created success', async () => {
+    const user = await create();
+    expect(user.id).toBeDefined();
+  });
+
+  it('should be find one user', async () => {
+    await create();
+    const result = await service.checkUserIsExist({ username: 'chenc' });
+    expect(result.id).toBeDefined();
   });
 });
