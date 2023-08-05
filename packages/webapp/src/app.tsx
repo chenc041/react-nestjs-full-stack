7b1997fce422146ef24983aaf2dc1bb56f797674
chenc <double_cl@163.com>
2023-08-05 21:56:02 +0800

    chore: update dependent (#69)

--- packages/webapp/src/app.tsx
+++ packages/webapp/src/app.tsx
@@ -1,34 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { routes } from '~/routes';
 import { Helmet } from 'react-helmet';
 import { GlobalContext } from '~/context';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Layout } from '~/pages/layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: routes.map((item) => {
+      const { pathname, component: Component } = item;
+      return {
+        path: pathname,
+        element: (
+          <>
+            <Helmet>
+              <title>{item.title}</title>
+            </Helmet>
+            <Component />
+          </>
+        ),
+      };
+    }),
+  },
+]);
+
 const App = () => {
   const [appName, setAppName] = React.useState('demo');
-
   return (
     <GlobalContext.Provider value={{ appName, setAppName }}>
-      <BrowserRouter>
-        <Routes>
-          {routes.map((item) => {
-            const { pathname, component: Component } = item;
-            return (
-              <Route
-                key={pathname}
-                path={pathname}
-                element={
-                  <>
-                    <Helmet>
-                      <title>{item.title}</title>
-                    </Helmet>
-                    <Component />
-                  </>
-                }
-              />
-            );
-          })}
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </GlobalContext.Provider>
   );
 };
