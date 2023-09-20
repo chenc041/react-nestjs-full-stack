ca12a3a3e88de9014d4622d7d0e918af2c932ac7
chenc <double_cl@163.com>
2023-09-20 10:44:12 +0800

    feat(exception): exception page add back to pre page

--- packages/webapp/src/exception/500.tsx
+++ packages/webapp/src/exception/500.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Result, Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 export default () => {
-  return <Result status="500" title="500" subTitle="对不起, 服务出错了" extra={<Button type="primary">返回上一页</Button>} />;
+  const navigate = useNavigate();
+  return (
+    <Result
+      status="500"
+      title="500"
+      subTitle="对不起, 服务出错了"
+      extra={
+        <Button type="primary" onClick={() => navigate(-1)}>
+          返回上一页
+        </Button>
+      }
+    />
+  );
 };
--- packages/webapp/src/exception/errorBoundary.tsx
+++ packages/webapp/src/exception/errorBoundary.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom';
+import { Button, Result } from 'antd';
 
 export const ErrorBoundary = () => {
-  const error = useRouteError();
+  const error = useRouteError() as { message: string };
   console.log(error, 'error');
-  return <div>ErrorBoundary</div>;
+  const navigate = useNavigate();
+  return (
+    <Result
+      status="500"
+      title="500"
+      subTitle={error.message || '出错啦!'}
+      extra={
+        <Button type="primary" onClick={() => navigate(-1)}>
+          返回上一页
+        </Button>
+      }
+    />
+  );
 };
